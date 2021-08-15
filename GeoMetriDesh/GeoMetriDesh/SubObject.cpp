325b2990dc58b058fdbad5d551ca50c28cc2cf69
DESKTOP-STU08M6\Admin <john987abc@naver.com>
2021-08-15 22:53:29 +0900

    플레이어 부분 수정

--- GeoMetriDesh/GeoMetriDesh/SubObject.cpp
+++ GeoMetriDesh/GeoMetriDesh/SubObject.cpp
@@ -4,9 +4,12 @@
 
 HRESULT SubObject::ReadObject()
 {
-    TextureManager::GetInstance()->InsertTexture(TextureManager::SINGLE, L"../Texture/Player/Player.png", L"Player");
+    // 텍스트 정보를 추가하고 불러야함
+     // 추가!
+    TextureManager::GetInstance()->InsertTexture(TextureManager::SINGLE, L"../Texture/Cube.png", L"Player");
     // 부르기!
     pTextInfo = TextureManager::GetInstance()->GetTextInfo(L"Player");
+    // 이미지들 경로 및 정보를 수기입력
 
     info.pos = { 500.f, 200.f, 0.f };
     info.size = { 100.f, 100.f, 0.f };
