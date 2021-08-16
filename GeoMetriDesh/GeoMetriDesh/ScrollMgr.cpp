5ca9d4b75f691ee492eabef2d4148bf592419644
DESKTOP-C5IG09C\pbk40 <pbk4008@naver.com>
2021-08-16 20:52:03 +0900

    땅 타일 완성

--- GeoMetriDesh/GeoMetriDesh/ScrollMgr.cpp
+++ GeoMetriDesh/GeoMetriDesh/ScrollMgr.cpp
@@ -2,7 +2,7 @@
 #include "ScrollMgr.h"
 
 IMPLEMENT_SINGLETON(CScrollMgr)
-CScrollMgr::CScrollMgr() : m_fScrollX(0.f), m_fScrollY(0.f)
+CScrollMgr::CScrollMgr() : m_fScrollX(0.f), m_fScrollY(0.f),m_fUpdateScrollX(0.f),m_fUpdateScrollY(0.f)
 {
 }
 
